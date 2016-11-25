5d50ec6caf5458ee150d1f9a209757b2134fa80d
kolja <koljaz@web.de>
2016-11-25 22:12:06 +0100

    Completed unit test for Pile class.

--- tests/Cards/PileTest.php
+++ tests/Cards/PileTest.php
@@ -2,6 +2,7 @@
 
 namespace CardTests;
 
+use Cards\Card;
 use Cards\Pile;
 use PHPUnit\Framework\TestCase;
 
@@ -11,5 +12,35 @@ class PileTest extends TestCase
 	{
 		$pile = new Pile();
 		$this->assertCount(0, $pile, 'Pile is not empty after creation.');
+	}
+
+	public function testOneCardCanBeAddedToPileAndFetched()
+	{
+		$mockBuilder = $this->getMockBuilder(Card::class);
+		$mockBuilder->disableOriginalConstructor();
+
+		$firstCard = $mockBuilder->getMock();
+
+		$pile = new Pile();
+		$pile->add($firstCard);
+		$this->assertSame([$firstCard], $pile->fetchAll(), 'Fetching one card from pile did not return the card.');
+	}
+
+	public function testMultipleCardsCanBeAddedToPileAndFetchedTogether()
+	{
+		$mockBuilder = $this->getMockBuilder(Card::class);
+		$mockBuilder->disableOriginalConstructor();
+
+		$firstCard = $mockBuilder->getMock();
+		$secondCard = $mockBuilder->getMock();
+
+		$pile = new Pile();
+		$pile->add($firstCard);
+		$pile->add($secondCard);
+		$this->assertSame(
+			[$firstCard, $secondCard],
+			$pile->fetchAll(),
+			'Fetching all cards from pile did not return all cards.'
+		);
 	}
 }
